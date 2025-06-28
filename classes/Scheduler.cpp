47d04c04ab02e71d8d0561ad4f07025464cf00c2
devonjavier <devonjarekjavier@gmail.com>
2025-06-28 15:38:32 +0800

    fix: merge conflict

---
+++ classes/Scheduler.cpp
@@ -0,0 +1,74 @@
+#include "process.cpp"
+#include <queue>
+#include <string>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+class Scheduler { 
+
+    private:
+    std::vector<Process> processes;
+    std::queue<Process> ready_queue; 
+    std::vector<Process> runningProcesses;
+    std::vector<Process> completedProcesses;
+    std::vector<std::thread> workerThreads;
+    std::atomic<bool> SchedulerRunning{false};
+    std::mutex queueMutex;
+    std::condition_variable queueCV;
+    std::string SchedulerType; 
+    int quantumCycles; 
+
+    public:
+    Scheduler(const std::string& Scheduler, int quantum) 
+        : SchedulerType(Scheduler), quantumCycles(quantum) {}
+
+    void addProcess(const Process& process) {
+        ready_queue.push(process);
+    }
+
+    void queueProcesses() {
+        std::unique_lock<std::mutex> lock(queueMutex);
+
+        for (auto& process : processes) {
+            // If the process is not yet queued or completed, add to ready queue
+            if (process.getState() == ProcessState::IDLE) {
+                process.setState(ProcessState::WAITING);
+                ready_queue.push(process);
+                queueCV.notify_one(); 
+            }
+        }
+    }
+
+    void startScheduler(int num_cpu) {
+        SchedulerRunning = true;
+
+    
+        for (int i = 0; i < num_cpu ; ++i) {
+            workerThreads.emplace_back(&Scheduler::SchedulerLoop, this);
+        }
+    }
+
+
+    bool isSchedulerRunning() const {
+        return SchedulerRunning;
+    }
+
+
+    // void FCFS() {
+    //     while (!processQueue.empty()) {
+    //         Process currentProcess = processQueue.front();
+    //         processQueue.pop();
+    //         currentProcess.setState(ProcessState::RUNNING);
+    //         runningProcesses.push_back(currentProcess);
+
+    //         // Simulate process execution
+    //         std::this_thread::sleep_for(std::chrono::milliseconds(currentProcess.getBurstTime()));
+    //         currentProcess.setState(ProcessState::COMPLETED);
+    //         completedProcesses.push_back(currentProcess);
+    //     }
+    // }
+};
+
--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include "os_interface.cpp"
 
 /*
-    BALINGIT, JAVIER, RAMOS, JIRO
+    BALINGIT, JAVIER, RAMOS
 
- * Task: Week 2 - Group Homework - Setting up your OS emulator
-    * ✅ Create your first-ever main menu. Use only the C++ built-in libraries.
-    * ✅ Provide your ASCII text header “CSOPESY” or a name for your command line emulator.
-    * Set up template code that accepts the following commands: initialize, screen, scheduler-test, scheduler-stop, report-util, clear, and exit. Simply print “X command recognized. Doing something.”, where X is the command recognized.
-    * For the clear command, once recognized, the screen clears, reprinting the header texts.
-    * For the exit command, the application/CLI immediately closes.
+ * Task: MCO1
  */
 
 
