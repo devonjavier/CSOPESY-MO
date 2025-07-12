ce5059fa650cc867fafdf7b5b6bb09097a10b5c3
aaaaandreiii <andrei.balingit.alab1@gmail.com>
2025-07-12 15:43:06 +0800

    fix: getters and setters for additional variables
    feat: added updateRunningAverage() function for every process

--- classes/process.h
+++ classes/process.h
@@ -28,8 +28,8 @@ private:
     uint64_t burst_time;            //this one as well
     uint64_t remaining_burst;       //i feel like this is necessary kasi
     uint64_t waiting_time;          //no need i think
-    uint64_t start_time[MAX_TIMERS];      //arbitrary size of 1024
-    uint64_t end_time[MAX_TIMERS];        //arbitrary size of 1024
+    uint64_t start_time[MAX];      //arbitrary size of 1024
+    uint64_t end_time[MAX];        //arbitrary size of 1024
     size_t run_count;
 
     int current_core_id;            //need -1 for unassigned core
@@ -43,6 +43,7 @@ public:
 
     void addInstruction(ICommand* instruction);
     void runInstructions();
+
     const std::vector<ICommand*>& getInstructions() const;
     uint16_t getPid() const;
     std::string getProcessName() const;
@@ -55,15 +56,24 @@ public:
     uint64_t Process::getStartTime(int index) const;
     uint64_t Process::getEndTime(int index) const;
     uint16_t Process::getRunCount() const;
-
     int getCurrentCoreId() const;
-    std::unordered_map<std::string, uint16_t> getVariables() const;
-    void setCurrentCoreId(int coreId);
     ProcessState getState() const;
-    void setState(ProcessState newState);
+    std::unordered_map<std::string, uint16_t> getVariables() const;
     uint16_t getVariableValue(const std::string& name);
+
+    void setBurstTime();
+    void setBurstTime(uint64_t burst);
+    void setRemainingBurst(uint64_t remaining);
+    void setWaitingTime(uint64_t waiting);
+    void setStartTime(uint64_t start);
+    void setEndTime(uint64_t end);
+    void setCurrentCoreId(int coreId);
+    void setState(ProcessState newState);
     void setVariable(const std::string& name, uint16_t value);
+
     std::string formatTime(const std::chrono::time_point<std::chrono::system_clock>& tp);
     void displayInstructionList();
     void displayVariables() const;
+
+    double updateRunningAverage(double previous_average, uint64_t new_wait, size_t index);
 };
